bfa89300f990669f1508d8f6238c67f138c4e9ba
Vladimir Lebedev <d3fl4t3@gmail.com>
2024-02-08 20:48:41 +0700

    Make expandable instruction table

--- tools/tvm-docs/src/components/InstructionTable.vue
+++ tools/tvm-docs/src/components/InstructionTable.vue
@@ -1,48 +1,82 @@
 <script setup lang="ts">
 import { cp0 } from 'tvm-spec';
+import VueMarkdown from 'vue-markdown-render'
 </script>
 
 <template>
-  <VaTabs v-model="value">
-    <template #tabs>
-      <VaTab v-for="tab in tabs" :key="tab" :name="tab">
-        {{ tab }}
-      </VaTab>
-    </template>
-    <div class="va-table-responsive">
-          <table class="va-table va-table--clickable">
-            <thead>
-              <tr>
-                <th>Mnemonic</th>
-                <th>Prefix</th>
-                <th>Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="insn in cp0.instructions.filter(insn => insn.doc.category == currentTab)" :key="insn.bytecode.prefix">
-                <td>{{ insn.mnemonic }}</td>
-                <td>{{ insn.bytecode.prefix }}</td>
-                <td style="max-width: 80rem; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ insn.doc.description }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-  </VaTabs>
+  <div class="flex flex-col h-screen justify-between bg-gray-50 dark:bg-black-700">
+    <main class="mb-auto mt-5 pb-10">
+      <VaTabs v-model="value" :style="{ '--va-tabs-align-items-horizontal': 'left' }">
+        <template #tabs>
+          <VaTab v-for="tab in tabs" :key="tab" :name="tab">
+            {{ tab }}
+          </VaTab>
+        </template>
+
+        <section class="py-10">
+          <div class="container mx-auto px-4 md:px-6">
+            <VaDataTable :items="cp0.instructions.filter(insn => insn.doc.category == currentTab)" :columns="columns"
+              clickable striped hoverable @row:click="({ row }) => row.toggleRowDetails()">
+              <template #cell(doc.description)="{ value }">
+                <vue-markdown :source="value"></vue-markdown>
+              </template>
+              <template #expandableRow="{ rowData }">
+                <div class="text-sm px-4 md:px-8 py-8 bg-white dark:bg-black-600">
+                  <div class="flex flex-col lg:flex-row">
+                    <div>
+                      <div>
+                        <vue-markdown :source="rowData.doc.description"></vue-markdown>
+                      </div>
+                      <pre class="py-4">{{ JSON.stringify(rowData, undefined, 4) }}</pre>
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </VaDataTable>
+          </div>
+        </section>
+      </VaTabs>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
-const categories = [...new Set(cp0.instructions.map(insn => insn.doc.category))]
+import { defineComponent } from "vue";
 
-export default {
-  data: () => ({
-    tabs: categories,
-    value: categories[0],
-  }),
+export default defineComponent({
+  components: {
+    VueMarkdown
+  },
+  data() {
+    const columns = [
+      { key: "bytecode.doc_opcode", label: 'opcode' },
+      { key: "mnemonic" },
+      { key: "doc.gas", width: '70px', label: 'gas' },
+      { key: "doc.description", label: 'description' },
+    ];
 
+    const categories = [...new Set(cp0.instructions.map(insn => insn.doc.category))];
+
+    return {
+      columns,
+      tabs: categories,
+      value: categories[0],
+    };
+  },
   computed: {
     currentTab() {
       return this.value;
     },
   },
-};
+});
 </script>
+
+<style>
+.va-data-table__table-tr--expanded td {
+  background: var(--va-background-border);
+}
+
+.va-data-table__table-expanded-content td {
+  background-color: var(--va-background-element);
+}
+</style>
