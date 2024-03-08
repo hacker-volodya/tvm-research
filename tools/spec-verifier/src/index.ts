c7487a41bf923b1d4de7253def73f804d73a1f6e
Vladimir Lebedev <d3fl4t3@gmail.com>
2024-03-08 20:20:36 +0700

    hints

--- tools/spec-verifier/src/index.ts
+++ tools/spec-verifier/src/index.ts
@@ -1,18 +1,14 @@
 import { cp0 } from "tvm-spec";
-import { addInstruction, generateInstructionOperands, runCode } from "./utils";
-import { beginCell } from "@ton/ton";
 
 (async () => {
-    let data: any = {};
     for (let instruction of cp0.instructions) {
-        if (instruction.mnemonic != "PUSHSLICE_REFS") continue;
-        let operands = generateInstructionOperands(instruction);
-        let builder = addInstruction(instruction, operands);
-        console.log(builder.endCell().toString())
-        let result = await runCode(beginCell().storeBuilder(builder).endCell(), []);
-        console.log(result);
-        data[instruction.mnemonic] = result;
+        for (let op of instruction.bytecode.operands) {
+            // @ts-ignore
+            if (op.display_hints == undefined) {
+                // @ts-ignore
+                op.display_hints = [];
+            }
+        }
     }
-    console.log("------------------")
-    console.log(JSON.stringify(data));
+    console.log(JSON.stringify(cp0));
 })();
